656ab5839672ee70828d97e9a169a91a01dad114
Zakky Aulia Aldrin <zakkyaulialdrin@gmail.com>
2026-05-31 11:43:42 +0700

    nambah seeder dosen

--- seeders/20260528000002-seed-admin-user.js
+++ seeders/20260528000002-seed-admin-user.js
@@ -1,7 +1,38 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const listDosen = [
+      { nama: 'Ricky Akbar, M.Kom', username: 'ricky', passwordKey: 'ricky123' },
+      { nama: 'Ullya Mega Wahyuni, M.Kom', username: 'ullya', passwordKey: 'ullya123' },
+      { nama: 'Afriyanti Dwi Kartika, S.Pd., M.T.', username: 'afriyanti', passwordKey: 'afriyanti123' },
+      { nama: 'Aina Hubby Aziira, M.Eng', username: 'aina', passwordKey: 'aina123' },
+      { nama: 'Nisa Dwi Angresti, M.Kom', username: 'nisa', passwordKey: 'nisa123' },
+      { nama: 'Husnil Kamil, M.T.', username: 'husnil', passwordKey: 'husnil123' },
+      { nama: 'Hafzatin Nurfatifa, M.Eng', username: 'hafzatin', passwordKey: 'hafzatin123' },
+      { nama: 'Dwi Welly Sukma Nirad, M.T.', username: 'dwi', passwordKey: 'dwi123' },
+      { nama: 'Adi Arga Arifnur, M.Kom', username: 'adi', passwordKey: 'adi123' },
+      { nama: 'Fajril Akbar, M.Sc', username: 'fajril', passwordKey: 'fajril123' },
+      { nama: 'Hasdi Putra, M.T.', username: 'hasdi', passwordKey: 'hasdi123' },
+      { nama: 'Jefril Rahmadoni, M.Kom', username: 'jefril', passwordKey: 'jefril123' },
+      { nama: 'Hafizah Hanim, M.Kom', username: 'hafizah', passwordKey: 'hafizah123' },
+      { nama: 'Haris Suryamen, M.Sc', username: 'haris', passwordKey: 'haris123' },
+      { nama: 'Prof. Dr. Surya Afnarius, S.T., M.Sc.', username: 'surya', passwordKey: 'surya123' },
+      { nama: 'Febby Apri Wenando, M.Kom', username: 'febby', passwordKey: 'febby123' },
+      { nama: 'Rahmatika Pratama Santi, M.T.', username: 'rahmatika', passwordKey: 'rahmatika123' }
+    ];
+
+    const dosenSeed = listDosen.map(d => ({
+      nama: d.nama,
+      username: d.username,
+      password: bcrypt.hashSync(d.passwordKey, 10),
+      role: 'dosen',
+      created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updated_at: Sequelize.literal('CURRENT_TIMESTAMP')
+    }));
+
     await queryInterface.bulkInsert('users', [
       {
         nama: 'Administrator',
@@ -19,30 +50,21 @@ module.exports = {
         created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
         updated_at: Sequelize.literal('CURRENT_TIMESTAMP')
       },
-      {
-        nama: 'Dosen Penguji 1',
-        username: 'dosen1',
-        password: '$2b$10$TnUy/Vt8jdDqPwoso8V/1OfosDb9csGrPiL6gKMMkIYp11vFvWJ36',
-        role: 'dosen',
-        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
-        updated_at: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      {
-        nama: 'Dosen Penguji 2',
-        username: 'dosen2',
-        password: '$2b$10$TnUy/Vt8jdDqPwoso8V/1OfosDb9csGrPiL6gKMMkIYp11vFvWJ36',
-        role: 'dosen',
-        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
-        updated_at: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
+      ...dosenSeed
     ], {
       ignoreDuplicates: true
     });
   },
 
   async down(queryInterface) {
+    const listUsernameDosen = [
+      'ricky', 'ullya', 'afriyanti', 'aina', 'nisa',
+      'husnil', 'hafzatin', 'dwi', 'adi', 'fajril',
+      'hasdi', 'jefril', 'hafizah', 'haris', 'surya',
+      'febby', 'rahmatika'
+    ];
     await queryInterface.bulkDelete('users', {
-      username: ['admin', '2311522022_laila', 'dosen1', 'dosen2']
+      username: ['admin', '2311522022_laila', ...listUsernameDosen]
     }, {});
   }
 };
